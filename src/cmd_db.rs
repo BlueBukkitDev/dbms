d775bac7fd1780d7cbc777553d63ea1806c89949
Jonathan Bruce <bluebukkitdev@gmail.com>
2023-11-18 16:11:06 -0500

    More tag considerations, started create command logic.

--- src/cmd_db.rs
+++ src/cmd_db.rs
@@ -2,8 +2,13 @@ use crate::help_msg::send_unknown_cmd;
 
 
 pub fn process_db(args:&[&str]) {
+    if(args.len() == 0){
+        //no args, return incorrect or help.
+    }
     if args[0].eq_ignore_ascii_case("create") {
-
+        if args.len() < 2 {
+            //return incorrect usage, must at least have a name
+        }
     }else if args[0].eq_ignore_ascii_case("delete") {
 
     }else if args[0].eq_ignore_ascii_case("select") {
@@ -11,5 +16,11 @@ pub fn process_db(args:&[&str]) {
     }else {
         let options = ["create", "delete", "select"];
         send_unknown_cmd("db", args, &options);
+    }
+}
+
+fn execute_create_cmd(args: &[&str]) {
+    if args[2].eq_ignore_ascii_case("-c"){
+
     }
 }
