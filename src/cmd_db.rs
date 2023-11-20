4822aa575ae7f2eac2fb5a0f1765bd148f272b49
bluebukkitdev <bluebukkitdev@gmail.com>
2023-11-20 08:51:24 -0500

    Adding general functions.

--- src/cmd_db.rs
+++ src/cmd_db.rs
@@ -2,17 +2,35 @@ use crate::help_msg::send_unknown_cmd;
 
 
 pub fn process_db(args:&[&str]) {
-    if(args.len() == 0){
+    if args.len() == 0 {
         //no args, return incorrect or help.
     }
     if args[0].eq_ignore_ascii_case("create") {
         if args.len() < 2 {
             //return incorrect usage, must at least have a name
+        }else if args.len() == 2 {
+            execute_create_cmd(args);
+        }else if args.len() == 3 {
+            if args[2].eq_ignore_ascii_case("-c") {
+                execute_create_custom_cmd(args);
+            }else {
+                //incorrect usage; wrong arg
+            }
+        }else {
+            //incorrect usage, too many args
         }
     }else if args[0].eq_ignore_ascii_case("delete") {
-
+        if args.len() == 2 {
+            execute_delete_cmd(args);//Should have a confirmation
+        }else {
+            //not enough or too many args
+        }
     }else if args[0].eq_ignore_ascii_case("select") {
-
+        if args.len() == 2 {
+            execute_select_cmd(args);
+        }else {
+            //not enough or too many args
+        }
     }else {
         let options = ["create", "delete", "select"];
         send_unknown_cmd("db", args, &options);
@@ -20,7 +38,17 @@ pub fn process_db(args:&[&str]) {
 }
 
 fn execute_create_cmd(args: &[&str]) {
-    if args[2].eq_ignore_ascii_case("-c"){
+    
+}
 
-    }
+fn execute_create_custom_cmd(args: &[&str]) {
+
+}
+
+fn execute_delete_cmd(args: &[&str]) {
+
+}
+
+fn execute_select_cmd(args: &[&str]) {
+
 }
