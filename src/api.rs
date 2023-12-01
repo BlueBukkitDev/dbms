9e0f6df23ddad4da8ce41450301bce56520f1080
Jonathan Bruce <bluebukkitdev@gmail.com>
2023-11-30 19:08:39 -0500

    Added sectioning to the tags file. Built out database creation & program files creation.

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,14 @@
 //This file will contain structs and impls for any operation needed. //zettlekasten query language, ZQL
 
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
 use std::fs;
 use crate::help_msg::*;
 use crate::cmd_db::*;
+
+//const tags: &[u8] = include_bytes!("tags.yml");//Need to be able to write existing files into the program folder on startup; save as arrays of bytes, compile, when executing, will
+//write to the file at the desired destination. EZPZ. 
 
 /**
  Session's purpose is to store relevant persistent data. For now this only stores the selected database, but may be expanded to 
@@ -17,6 +23,7 @@ impl Session {
      Creates a new session with a null database
      */
     pub fn new() -> Session {
+        setup_program_files();
         Session {
             selected_db : String::new()
         }
@@ -53,9 +60,11 @@ impl API {
     pub fn select_db (&mut self, name: &str) {
         self.session.select_db(name)
     }
+
     pub fn create_db (&mut self, name: &str) {
 
     }
+
     pub fn create_db_at_path (&mut self, name: &str, path: String) {
 
     }
@@ -81,8 +90,10 @@ fn process_command(cmd: &str, args: &[&str]) -> bool {
         
     }else if cmd == "write" {
         if args.len() == 1 {
-            create_folder(&args[0].to_string());
+            //create_folder(&args[0].to_string());
+            
         }else {
+            setup_std_db("Rando", "C:\\Users\\be\\Desktop")
             //send wrong args msg
         }
     }else if cmd == "put" {
@@ -95,18 +106,51 @@ fn process_command(cmd: &str, args: &[&str]) -> bool {
     return false;
 }
 
-pub fn setup_std_db(name:&str, path:&str) {
-    let new_path = format!("{}/{}", path, name);
-    create_folder(&new_path);
-    create_folder(&format!("{}/Person", new_path));
-    create_folder(&format!("{}/Location", new_path));
-    create_folder(&format!("{}/Event", new_path));
+fn get_root() -> Result<PathBuf, std::io::Error> {
+    let current_dir = env::current_dir()?;
+    Ok(current_dir.ancestors().last().unwrap_or_else(|| Path::new("/")).to_path_buf())
+}
+
+fn setup_program_files() {
+    if program_is_setup() {
+        return;
+    }
+    //if not, create a bunch of files with configs and whatnot. 
+    let root = get_program_folder_path();
+    create_folder(&root);
+
+}
+
+fn program_is_setup () -> bool {
+    Path::new(&get_program_folder_path()).exists()
+}
+
+fn get_program_folder_path () -> String {
+    let root = get_root().expect("Error finding root folder");
+    let path = root.join("EZQL");
+    let folder = path.to_str().expect("Failed to convert path to string");
+    folder.to_string()
+}
+
+fn setup_std_db(name:&str, path:&str) {
+    let db_path = Path::new(path).join(name);
+    let db_folder = db_path.to_str().expect("Failed to convert path to string");
+    create_folder(&db_folder);
+    create_folder(&format!("{}\\Person", db_folder));
+    create_folder(&format!("{}\\Location", db_folder));
+    create_folder(&format!("{}\\Concept", db_folder));
+    create_folder(&format!("{}\\Technology", db_folder));
+    create_folder(&format!("{}\\Event", db_folder));
+    create_folder(&format!("{}\\Creature", db_folder));
+    create_folder(&format!("{}\\Object", db_folder));
+    create_folder(&format!("{}\\Structure", db_folder));
+    println!("Database has been created.");
 }
 
 //This fn is here for future use
-pub fn create_folder(path: &String) {
+fn create_folder(path: &str) {
     match fs::create_dir(path) {
-        Ok(_) => println!("Directory created successfully"),
+        Ok(_) => return,
         Err(e) => println!("Error creating directory: {:?}", e),
     }
 }
